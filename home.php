e6a7dcbf67117c1beae332293b4b1e531e1fa473
Vheshan37 <vihangaheshan37@gmail.com>
2023-09-05 10:44:26 +0530

    Home page Updates

--- home.php
+++ home.php
@@ -10,7 +10,6 @@
   <link rel="stylesheet" href="./css/bootstrap.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="./css/home.css">
-  <!-- <link rel="stylesheet" href="../"> -->
 </head>
 
 <body onload="homeOnloadFunction();" class="c-default">
