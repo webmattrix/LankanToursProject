ac02acc7463afdf77b5ca3973993de220aa7b405
Vheshan37 <vihangaheshan37@gmail.com>
2023-09-09 11:11:21 +0530

    Home updates & footer component

---
+++ components/footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <div class="col-12 position-sticky top-100">
+        <div class="row">
+
+            <footer class="footer p-2">
+                <div class="col-12">
+                    <div class="text-uppercase segoeui-bold text-white fs-4">Lankan Tours</div>
+                    <div class="quicksand-Medium text-white-50 ps-3">Company Slogan</div>
+                </div>
+                <div class="d-flex justify-content-between mt-4 px-3">
+
+                    <div class="quicksand-SemiBold w-100">
+                        <span class="fs-5 text-white">Useful Pages</span>
+                        <ul class="d-flex flex-column gap-1">
+                            <li class="text-white"><a href="#" class="text-decoration-none text-white">About</a></li>
+                            <li class="text-white"><a href="#" class="text-decoration-none text-white">Contact</a></li>
+                            <li class="text-white"><a href="#" class="text-decoration-none text-white">Services</a></li>
+                            <li class="text-white"><a href="#" class="text-decoration-none text-white">Message & Request</a></li>
+                            <li class="text-white"><a href="#" class="text-decoration-none text-white">Gallery</a></li>
+                        </ul>
+                    </div>
+                    <div class="quicksand-SemiBold w-100">
+                        <span class="fs-5 text-white">Follow US</span>
+                        <div class="d-flex gap-3 pt-2">
+                            <iconify-icon icon="ic:sharp-facebook" class="text-white fs-3"></iconify-icon>
+                            <iconify-icon icon="entypo-social:youtube-with-circle" class="text-white fs-3"></iconify-icon>
+                            <iconify-icon icon="entypo-social:linkedin-with-circle" class="text-white fs-3"></iconify-icon>
+                            <iconify-icon icon="formkit:twitter" class="text-white fs-3"></iconify-icon>
+                            <iconify-icon icon="ri:whatsapp-fill" class="text-white fs-3"></iconify-icon>
+                        </div>
+                    </div>
+                    <div class="quicksand-SemiBold w-100">
+                        <span class="fs-5 text-white">Request & Complain</span>
+                    </div>
+
+                </div>
+            </footer>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
