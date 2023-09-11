d39f85f9db692cfe80f79b84cee489a5a4291f0a
Vheshan37 <vihangaheshan37@gmail.com>
2023-09-11 06:59:49 +0530

    Update htaccess file & README file

---
+++ adminPanel.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/adminTemplate.css">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="d-flex p-0">
+                <?php
+                include "./components/adminSidebar.php"; // change if you using other component like "guideSidebar.php"
+                ?>
+
+                <div class="d-flex w-100 flex-column" style="max-height: 100vh; overflow-y: auto;">
+                    <?php
+                    include "./components/adminHeader.php"; // change if you using other component like "guideHeader.php"
+                    ?>
+
+                    <!-- Page Content / body content eka methanin liyanna -->
+                    <div class="col-12">
+
+                    </div>
+                    <!-- Page Content / body content eka methanin liyanna -->
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <script src="./js/adminTemplate.js"></script>
+    <script src="./js/bootstrap.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</body>
+
+</html>
