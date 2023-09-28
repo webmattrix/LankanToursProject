157467743fe2f4bb19a35747c066c2fb5f6b88f8
Lakshan18 <lakshanvlogs3276@gmail.com>
2023-09-28 19:56:42 +0530

    continue watchlist

--- manageTours.php
+++ manageTours.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin | Manage Tour</title>
     <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./css/adminTemplate.css">
     <link rel="stylesheet" href="./css/manageTours.css" />
---
+++ watchlist.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Watchlist</title>
+    <link rel="stylesheet" href="./css/bootstrap.css"/>
+    <link rel="stylesheet" href="./css/watchlist.css"/>
+</head>
+<body>
+
+ <div class="container-fluid">
+    <div class="row">
+        
+    </div>
+ </div>
+    
+</body>
+</html>
