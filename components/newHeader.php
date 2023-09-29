5e41a13d3fc3ff24748a38aafe64679c48a242d0
Vheshan37 <vihangaheshan37@gmail.com>
2023-09-29 22:51:43 +0530

    Create home page image sliders

--- components/newHeader.php
+++ components/newHeader.php
@@ -63,7 +63,7 @@
                         <div class="d-flex justify-content-center">
                             <img src="./assets/img/flag.svg" alt="Helolo" style="width: 50px; clip-path: circle();">
                         </div>
-                        <div class="mt-3 d-flex align-items-center gap-2 header-more-panel">
+                        <div class="mt-3 d-flex align-items-center gap-2 header-more-panel" id="goToProfile">
                             <iconify-icon icon="iconamoon:profile-circle-fill"></iconify-icon>
                             <span class="x">Profile</span>
                         </div>
@@ -71,7 +71,7 @@
                             <iconify-icon icon="mdi:heart"></iconify-icon>
                             <span class="x">Watchlist</span>
                         </div>
-                        <div class="mt-1 d-flex align-items-center gap-2 header-more-panel">
+                        <div class="mt-1 d-flex align-items-center gap-2 header-more-panel"> 
                             <iconify-icon icon="mdi:flight"></iconify-icon>
                             <span class="x">My Tours</span>
                         </div>
--- profile.php
+++ profile.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="./css/profile.css">
 </head>
 
-<body class="c-default" style="background-color: #E7E7E7;">
+<body class="c-default" style="background-color: #DCDCDC;">
 
     <body onload="homeOnloadFunction();" class="c-default" style="background-color: #E7E7E7;">
 
@@ -34,12 +34,15 @@
                             <label for="profileImage" class="position-absolute end-0 top-50">
                                 <iconify-icon icon="solar:camera-bold"></iconify-icon>
                             </label>
+                            <!-- Image Uploader for profile image -->
                             <input type="file" name="" id="profileImage" class="d-none">
+                            <!-- Image Uploader for profile image -->
                         </div>
 
                         <div class="d-flex w-100 justify-content-between position-absolute top-0 start-0 px-3 pt-3 profile_header_icon_container">
-                            <span>
-                                <iconify-icon icon="uil:arrow-left"></iconify-icon>
+
+                            <span id="backToHome">
+                                <iconify-icon icon="uil:arrow-left"></iconify-icon> <!-- Back to home button -->
                             </span>
                             <label for="profileBackground">
                                 <iconify-icon icon="solar:camera-bold"></iconify-icon>
@@ -51,12 +54,15 @@
                         </div>
                     </div>
 
+                    <!-- Profile Content -->
                     <div class="profile_content d-flex justify-content-center quicksand-SemiBold">
                         <div class="">
                             <div class="d-flex flex-column align-items-center">
                                 <iconify-icon icon="ep:user-filled" style="font-size: 50px;"></iconify-icon>
                                 <span class="quicksand-SemiBold fs-5">Profile</span>
                             </div>
+
+                            <!-- Profile Card -->
                             <div class="quicksand-Medium">
                                 <div class="d-flex flex-column mt-3">
                                     <label for="">Name</label>
@@ -83,15 +89,101 @@
                                     <input type="text" disabled value="England" class="bg-transparent border-0 border-bottom text-center mt-1" />
                                 </div>
                             </div>
+                            <!-- Profile Card -->
+
                         </div>
                     </div>
+                    <!-- Profile Content -->
+
+
+                    <!-- Profile setting content -->
+                    <div class="my-3 px-4">
+                        <div class="profile-setting-container px-3">
+                            <div class="header d-flex justify-content-between quicksand-Bold py-1 align-items-center" id="profileSettingHeader">
+                                <span>Settings</span>
+                                <iconify-icon icon="mingcute:down-line" class="fs-5" id="profileSettingIcon"></iconify-icon>
+                            </div>
+                            <div class="body quicksand-SemiBold" id="profileSettingBody" data-position="up">
+                                <ul class="list-unstyled pt-3 pb-2">
+                                    <li class="">
+                                        <iconify-icon icon="material-symbols:security"></iconify-icon>
+                                        <span>Security</span>
+                                        <ul class="list-unstyled ps-4">
+                                            <li><span>Change Password</span></li>
+                                            <li><span>Forgot Password</span></li>
+                                        </ul>
+                                    </li>
+                                    <li class="mt-2">
+                                        <iconify-icon icon="bxs:notification"></iconify-icon>
+                                        <span>Notification</span>
+                                    </li>
+                                    <li class="mt-2">
+                                        <iconify-icon icon="ic:round-account-box"></iconify-icon>
+                                        <span>Account</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Profile setting content -->
+
+                    <!-- Change Profile Content -->
+                    <div class="my-3 px-4">
+                        <div class="profile-change-container px-3">
+                            <div class="header d-flex justify-content-between quicksand-Bold py-1 align-items-center" id="profileChangeHeader">
+                                <span>Change Profile</span>
+                                <iconify-icon icon="mingcute:down-line" class="fs-5" id="profileChangeIcon"></iconify-icon>
+                            </div>
+                            <div class="body quicksand-SemiBold" id="profileChangeBody" data-position="down" style="height: auto;">
+                                <div class="pt-3 pb-2">
+                                    <div class="d-flex flex-column">
+                                        <label for="">Name</label>
+                                        <input type="text" />
+                                    </div>
+                                    <div class="d-flex flex-column">
+                                        <label for="">Mobile</label>
+                                        <input type="tel" />
+                                    </div>
+                                    <div class="d-flex flex-column">
+                                        <label for="">Date of Birth</label>
+                                        <input type="date" />
+                                    </div>
+                                    <div class="d-flex flex-column">
+                                        <label for="">Gender</label>
+                                        <select>
+                                            <option>Select</option>
+                                            <option>Male</option>
+                                            <option>Female</option>
+                                        </select>
+                                    </div>
+                                    <div class="d-flex flex-column">
+                                        <label for="">Country</label>
+                                        <select>
+                                            <option>Select</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="w-100 d-flex justify-content-end pb-2">
+                                    <button class="btn btn-secondary px-4">Cancel</button>
+                                    <button class="btn btn-success px-4">Change</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Change Profile Content -->
 
                 </div>
 
             </div>
+
+            <?php include "./components/footer.php"; ?>
+
         </div>
     </body>
 
+    <script src="./js/profile.js"></script>
+    <script src="./js/newHeader.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 </body>
 
 </html>
