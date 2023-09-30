78232168d8b751c4eb3f6fc5832f44ae07baa5ba
sachintha <madsachintha1234@gmail.com>
2023-09-30 10:09:34 +0530

    few changes

--- itinerary.php
+++ itinerary.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Itinerary Page</title>
 
-    <link rel="stylesheet" href="../css/bootstrap.css" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href="../css/itinerary.css" />
-    <link rel="stylesheet" href="../css/scrolbar.css">
+    <link rel="stylesheet" href="css/itinerary.css" />
+    <link rel="stylesheet" href="css/scrolbar.css">
 
 </head>
 
