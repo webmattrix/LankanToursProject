c7ec88c00890d7c7e81e732b93ab01bacfba1cde
sachintha <madsachintha1234@gmail.com>
2023-10-15 15:46:23 +0530

    dark theme added

--- places.php
+++ places.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home | Places</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- apply if it is dark theme -->
+    <!-- <link rel="stylesheet" href="css/placesDark.css"> -->
     <link rel="stylesheet" href="css/places.css">
     <link rel="stylesheet" href="./css/newHeader.css" />
     <link rel="stylesheet" href="./css/footer.css" />
@@ -14,7 +16,7 @@
 
 <body>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row background">
             <?php include "./components/newHeader.php"; ?>
 
             <div class="col-12">
@@ -25,7 +27,7 @@
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                         </ol>
-                        <div class="carousel-inner w-100  p-0">
+                        <div class="carousel-inner p-0">
                             <div class="carousel-item active">
                                 <div style="position: relative;">
                                     <img class="d-block w-100" src="./assets/img/itinerary_IMG/matara.jpg" alt="Second slide">
@@ -61,62 +63,72 @@
 
             <div class="row">
 
-                <div class="col-12 px-4">
-                    <p class="with-line mt-4">Most Famous Cities<span class="line"></span></p>
-                </div>
-
                 <div class="col-12">
-                    <div class="row justify-content-center text-center">
-                        <div class="col-12">
-                            <div class="row align-items-center p-4">
-                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
-                                    <div class="card1">
-                                        <div class="card1-inner">
-                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
-                                            <div class="card1-content">
-                                                <p>Matara</p>
+                    <div class="row city_container p-3">
+                        <div class="col-12 section">
+                            <div class="row">
+
+                                <div class="col-12 px-4">
+                                    <p class="with-line mt-4">Most Famous Cities<span class="line"></span></p>
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="row justify-content-center text-center">
+                                        <div class="col-12">
+                                            <div class="row align-items-center p-4">
+                                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
+                                                    <div class="card1">
+                                                        <div class="card1-inner">
+                                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
+                                                            <div class="card1-content">
+                                                                <p>Matara</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
+                                                    <div class="card1">
+                                                        <div class="card1-inner">
+                                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
+                                                            <div class="card1-content">
+                                                                <p>Matara</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
+                                                    <div class="card1">
+                                                        <div class="card1-inner">
+                                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
+                                                            <div class="card1-content">
+                                                                <p>Matara</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
+                                                    <div class="card1">
+                                                        <div class="card1-inner">
+                                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
+                                                            <div class="card1-content">
+                                                                <p>Matara</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
-                                    <div class="card1">
-                                        <div class="card1-inner">
-                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
-                                            <div class="card1-content">
-                                                <p>Matara</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
-                                    <div class="card1">
-                                        <div class="card1-inner">
-                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
-                                            <div class="card1-content">
-                                                <p>Matara</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center align-items-center py-3">
-                                    <div class="card1">
-                                        <div class="card1-inner">
-                                            <img class="card1-img" src="./assets/img/itinerary_IMG/matara.jpg" alt="Image">
-                                            <div class="card1-content">
-                                                <p>Matara</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="col-12">
-                    <div class="row city_container p-3" style="background-color:#dedede">
-                        <div class="col-12" style="background-color:#ffffff">
+                    <div class="row city_container p-3">
+                        <div class="col-12 section">
                             <div class="row">
                                 <div class="col-12 mt-4">
                                     <div class="col-10 col-md-8 col-lg-6 offset-lg-3 offset-md-2 offset-1 city_image">
