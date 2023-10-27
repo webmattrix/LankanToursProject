20c996f3fc01fd032a6247aae56aa4cd9f9a6654
Vheshan37 <vihangaheshan37@gmail.com>
2023-10-27 10:19:01 +0530

    GetOrder API

--- assets/model/getOrdersList.php
+++ assets/model/getOrdersList.php
@@ -76,25 +76,3 @@ class getOrders
         return ($responseArray);
     }
 }
-
-$date = new DateTime();
-$today = $date->setTimezone(new DateTimeZone("Asia/Colombo"));
-$today = $today->format("Y-m-d");
-
-$order_query = "SELECT *,`tour`.`name` AS `tour_name`,`employee`.`name` AS `guide_name` FROM `order` 
-INNER JOIN `tour` ON `tour`.`id`=`order`.`tour_id` 
-INNER JOIN `guide` ON `guide`.`id`=`order`.`guide_id` 
-INNER JOIN `employee` ON `employee`.`id`=`guide`.`employee_id` 
-WHERE `order`.`start_date` <= '" . $today . "' AND `order`.`end_date` >= '" . $today . "' 
-ORDER BY `start_date` ASC";
-
-$custom_order_query = "SELECT *,`employee`.`name` AS `guide_name` FROM `custom_tour`
-INNER JOIN `guide` ON `guide`.`id`=`custom_tour`.`guide_id`
-INNER JOIN `employee` ON `employee`.`id`=`guide`.`employee_id`
-WHERE `custom_tour`.`start_date` <= '" . $today . "' AND `custom_tour`.`end_date` >= '" . $today . "' ORDER BY `start_date` ASC";
-
-$orderList = getOrders::getOrderList($order_query, $custom_order_query);
-
-for ($x = 0; $x < sizeof($orderList); $x++) {
-    echo (json_encode($orderList[$x]));
-}
